003318aab2a2f17a0f78aad28921eed001b2601b
Alejandro López Sánchez <alejandrols@inditex.com>
2021-08-07 14:51:19 +0200

    feat(migrations): add new migration to generate random data for working inputs

---
+++ migrations/1.0.0/03-add-inputs.js
@@ -0,0 +1,29 @@
+const faker = require("faker");
+
+const up = async db => {
+  const EIGHT_HOURS_IN_MILLISECONDS = 1000 * 60 * 60 * 8;
+
+  const users = await db.collection("users").find({}).toArray();
+  const inputs = [];
+
+  users.forEach(user => {
+    for (let i = 1; i < 32; i++) {
+      const startAt = +new Date(2021, 6, i, faker.datatype.number({ min: 7, max: 10 }), 0, 0, 0);
+      const endAt = +new Date(2021, 6, i, faker.datatype.number({ min: 15, max: 22 }), 0, 0, 0);
+
+      const workingTime = endAt - startAt;
+      const overtime = workingTime > EIGHT_HOURS_IN_MILLISECONDS ? workingTime - EIGHT_HOURS_IN_MILLISECONDS : 0;
+
+      inputs.push({
+        startAt,
+        endAt,
+        overtime,
+        user: user._id
+      });
+    }
+  });
+
+  await db.collection("inputs").insertMany(inputs);
+};
+
+module.exports = { up };

cb37bd9cc865b9646c2b29acf4c7685b98e4bfd0
Alejandro López Sánchez <alejandrols@inditex.com>
2021-08-08 05:34:05 +0200

    chore(migrations): tweak dates

--- migrations/1.0.0/03-add-inputs.js
+++ migrations/1.0.0/03-add-inputs.js
@@ -9,7 +9,7 @@ const up = async db => {
   users.forEach(user => {
     for (let i = 1; i < 32; i++) {
       const startAt = +new Date(2021, 6, i, faker.datatype.number({ min: 7, max: 10 }), 0, 0, 0);
-      const endAt = +new Date(2021, 6, i, faker.datatype.number({ min: 15, max: 22 }), 0, 0, 0);
+      const endAt = +new Date(2021, 6, i, faker.datatype.number({ min: 15, max: 20 }), 0, 0, 0);
 
       const workingTime = endAt - startAt;
       const overtime = workingTime > EIGHT_HOURS_IN_MILLISECONDS ? workingTime - EIGHT_HOURS_IN_MILLISECONDS : 0;
